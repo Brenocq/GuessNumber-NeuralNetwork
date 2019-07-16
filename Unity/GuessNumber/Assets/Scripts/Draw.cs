e16f2b1463de7322dc6bc4d3fffcbb19fe892e3a
BrenoQueiroz <breno1423@gmail.com>
2019-07-15 22:09:06 -0300

    Feat: Boolean matrix

--- Unity/GuessNumber/Assets/Scripts/Draw.cs
+++ Unity/GuessNumber/Assets/Scripts/Draw.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class Draw : MonoBehaviour {
 	[SerializeField] GameObject pixel;
+	bool[,] canvas = new bool[51, 51];
+	List<GameObject> pixels;
 	private bool mousePressed;
+
+
 	void Start () {
 	}
 
-
 	void Update () {
+		 draw();
+	}
+
+	private void draw(){
 		// Mouse X and Y in the canvas grid (center of pixels)
 		if(Input.GetMouseButtonDown(0)){
 			mousePressed = true;
@@ -17,10 +24,11 @@ public class Draw : MonoBehaviour {
 		if(Input.GetMouseButtonUp(0)){
 			mousePressed = false;
 		}
+
 		if(mousePressed){
 			float mouseX = ((Input.mousePosition.x/Screen.width-0.5f)  * 10f*16f/9f+4f) /8f*50f;
 			float mouseY = ((Input.mousePosition.y/Screen.height-0.5f) * 10f+4f)			  /8f*50f;
-			Debug.Log("("+mouseX+","+mouseY+")");
+			//Debug.Log("("+mouseX+","+mouseY+")");
 			createPixel((int)mouseX,(int)mouseY);
 			createPixel((int)mouseX+1,(int)mouseY);
 			createPixel((int)mouseX-1,(int)mouseY);
@@ -29,7 +37,7 @@ public class Draw : MonoBehaviour {
 		}
 	}
 
-	public void createPixel(int x, int y){
+	private void createPixel(int x, int y){
 		float posX = -4f+(8f/51f)*(x+0.5f);
 		float posY = -4f+(8f/51f)*(y+0.5f);
 		if((x>=0 && x<51)&&(y>=0 && y<51)){
@@ -38,6 +46,19 @@ public class Draw : MonoBehaviour {
 			GameObject goPixel;
 			goPixel = Instantiate(pixel, pos, rot) as GameObject ;
 			goPixel.transform.parent = transform;
+			//pixels.Add(goPixel);
+
+			// Update canvas matrix
+			canvas[x,y] = true;
 		}
+	}
+
+	public void clearCanvas(){
+		for (int i=0; i<canvas.GetLength(0); i++) {
+			for (int j=0; j<canvas.GetLength(1); j++) {
+				canvas[i,j]=false;
+			}
+		}
+		//Destroy(pixels[0]);
 	}
 }
